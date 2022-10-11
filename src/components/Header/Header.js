7728bbf8a5f76d7e7004031cf3ecc21793425a37
md rafiqul islam <rafiqulislam11002@gmail.com>
2022-10-11 10:58:16 +0600

    header responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,9 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className='py-8 flex justify-between items-center px-5 shadow-lg'>
+        <nav className='py-8 flex flex-col md:flex-row md:justify-between md:items-center px-5 shadow-lg'>
             <div>
-                <Link to='/home'><img className='w-64 h-auto' src={logo} alt="" /></Link>
+                <Link to='/home'><img className='w-64 h-auto mx-auto' src={logo} alt="" /></Link>
             </div>
             <div>
                 <NavLink className={({ isActive }) => isActive ? 'text-blue-900 font-semibold mr-5 text-2xl' : 'mr-5 text-2xl'} to='/home'>Home</NavLink>
