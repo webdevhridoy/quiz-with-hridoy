025e5ada494979586eedc7e724500ac50fb58ef3
md rafiqul islam <rafiqulislam11002@gmail.com>
2022-10-12 19:18:51 +0600

    margin fixed

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,6 +1,5 @@
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Swal from 'sweetalert2';
--- src/components/SingleQuizs/SingleQuizs.js
+++ src/components/SingleQuizs/SingleQuizs.js
@@ -9,7 +9,7 @@ const SingleQuizs = () => {
 
     return (
         <div>
-            <div className='my-20 bg-violet-700 flex justify-center items-center py-28 md:py-40'>
+            <div className='mb-20 bg-violet-700 flex justify-center items-center py-28 md:py-40'>
                 <h2 className='text-3xl text-white underline'>Quiz of <span className=' font-semibold'>{name}</span></h2>
             </div>
             <div>
